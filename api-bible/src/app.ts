2940080a1e2dac55a05a2ab5dd68179cdf99b511
krystian john dumapit <kjedumapit@gmail.com>
2026-04-28 01:18:03 +0800

    feat: add specific cors

--- api-bible/src/app.ts
+++ api-bible/src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import hpp from 'hpp';
-import { rateLimit } from 'express-rate-limit';
-import bibleRoutes from './routes/bibleRoutes';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import { rateLimit } from "express-rate-limit";
+import bibleRoutes from "./routes/bibleRoutes";
 
 const app = express();
 
@@ -15,18 +15,27 @@ app.use(hpp());
 
 // 3. Rate Limiting (Prevent Brute Force/DoS)
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
+  windowMs: 5 * 60 * 1000, // 5 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: 'Too many requests from this IP, please try again after 1 minute',
+  message: "Too many requests from this IP, please try again after 1 minute",
 });
 
 // Apply rate limiter to all API routes
-app.use('/api', limiter);
+app.use("/api", limiter);
 
-app.use(cors());
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:1111"
+      : "https://my-one-bible.netlify.app",
+  methods: ["GET"], // Allow only these methods
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use('/api/bible', bibleRoutes);
+app.use("/api/bible", bibleRoutes);
 
 export default app;
